d79cabc0261e7ddd8cf32a3786b0cb249aef68c6
Sravani Raparthi <sravaniraparthis24@gmail.com>
2026-05-09 21:53:16 +0530

    Updated file paths to relative paths

--- src/test/java/com/utility/JSONUtility.java
+++ src/test/java/com/utility/JSONUtility.java
@@ -15,7 +15,7 @@ public class JSONUtility {
 
 		Gson gson = new Gson();
 
-		File jsonFile = new File(System.getProperty("user.dir") + "/config/config.json");
+		File jsonFile = new File(System.getProperty("user.dir") + "//config//config.json");
 		FileReader fileReader = null;
 		
 		try {
